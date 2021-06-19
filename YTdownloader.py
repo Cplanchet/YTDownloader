cb77bfd20909bf228d5208a114b620d1fb13680d
Caleb Planchet <masterblasterplanchet@gmail.com>
2021-06-19 12:32:08 -0400

    Initial Commit

---
+++ YTdownloader.py
@@ -0,0 +1,20 @@
+from pytube import YouTube
+import sys
+
+if len(sys.argv) == 2:   
+    try:
+        YouTube(sys.argv[1]).streams.first().download()
+        print("download Complete")
+    except:
+        print("failed to download")
+elif len(sys.argv) == 3:
+    if sys.argv[2] == "-a":
+        try:
+            YouTube(sys.argv[1]).streams.get_audio_only().download()
+            print("Download Complete")
+        except:
+            print("Failed to Download")
+    else:
+        print("Invalid Arguments")
+else:
+    print("Invalid Arguments")
