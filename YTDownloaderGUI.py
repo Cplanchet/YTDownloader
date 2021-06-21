08e49c8bbadd4af4ec2fc4a737d9000ca7078d80
Caleb Planchet <masterblasterplanchet@gmail.com>
2021-06-21 15:32:00 -0400

    added a label, entry, button, and initilized button logic

--- YTDownloaderGUI.py
+++ YTDownloaderGUI.py
@@ -1,8 +1,43 @@
 from tkinter import *
-import pytube
+from tkinter import filedialog as fd
+from tkinter import messagebox as mb
+from pytube import *
+
+def onButtonPress():
+    path = fd.asksaveasfilename()
+    
+    temp = path.split("/")
+    last = len(temp)
+    fileName = temp[last-1]
+    print(fileName)
+
+    print(UrlInput.get())
+    try:
+        pass
+        yt = YouTube(UrlInput.get())
+    except:
+       pass
+       mb.showerror("Ru Ro Raggy", "Invalid URL")
+
+
 
 root = Tk()
 root.title("YouTube Download Tool")
-root.geometry("1920x1080")
 root.iconbitmap('res/YTDownloader_Icon.ico')
+
+frame = Frame(root)
+frame.pack()
+
+frameBot = Frame(root)
+frameBot.pack(side=BOTTOM)
+
+UrlLabel = Label(frame, text="URL")
+UrlLabel.pack(side=LEFT)
+
+UrlInput = Entry(frame)
+UrlInput.pack(side=LEFT)
+
+EnterButton = Button(frameBot, command= onButtonPress, text = "Download as...")
+EnterButton.pack()
 root.mainloop()
+
